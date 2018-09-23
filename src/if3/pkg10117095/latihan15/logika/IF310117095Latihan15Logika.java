f515975a1bf509e5b6cdf782ad6fdcc3ab4d9e9c
alvinlukmanNH <galinx20gmail.com>
2018-09-23 20:00:19 +0700

    Logika

---
+++ src/if3/pkg10117095/latihan15/logika/IF310117095Latihan15Logika.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if3.pkg10117095.latihan15.logika;
+
+/**
+ *
+ * @author 
+ * NAMA         : Alvin Lukman Nulhakim
+ * KELAS        : IF-3
+ * NIM          : 10117095
+ * Deklarasi    : Program ini berisi fungsi Logika Boolean ( True & False )
+ */
+public class IF310117095Latihan15Logika {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        boolean a = true;
+        boolean b = false;
+        System.out.println("a && b = " + (a && b));
+        System.out.println("a || b = " + (a || b));
+        System.out.println("!a = " + !a);
+        System.out.println("!(a && b) = " + !(a && b));
+    }
+    
+}
